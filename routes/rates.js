cefcf575bd45dbb9e24c663542d7dba946d7e87f
Deji Adegbite <d2alphame@yahoo.com>
2021-01-28 04:14:32 +0100

    feat(rates): Correct error with undefined 'base' in query

--- routes/rates.js
+++ routes/rates.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const https = require('https');
+const querystring = require('querystring');
 
 var router = express.Router();
 
@@ -7,14 +8,29 @@ var url = 'https://api.exchangeratesapi.io/latest'
 
 router.get('/', function(req, res, next) {
 
-	url += '?base=' + req.query.base + '&symbols=' + req.query.currency
+	// If there's a 'currency' parameter in the query object, then change it into 'symbols'
+	// The exhange rate api uses symbols as against currency in this case.
+	if('currency' in req.query) {
+		req.query['symbols'] = req.query['currency']
+		delete req.query.currency
+	}
+
+	// querystring to convert the query object into a query string to attach to the url
+	url += '?' + querystring.stringify(req.query)
+
 	https.get(url, (resp) => {
 		let data = ''
   		resp.on('data', (chunk) => {
 			data += chunk
 		});
 		resp.on('end', () => {
-			res.json({ results: JSON.parse(data)})
+			let results = JSON.parse(data)
+
+			// The request completes but there's an error probably because of bad request
+			if(results.error) {
+				res.json(results)
+			}
+			else { res.json({ results: results }) }
 		})  
 
 	}).on('error', (e) => {
