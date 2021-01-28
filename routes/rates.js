b89ecc80e3d83f0514e66721b927680d8148278e
Deji Adegbite <d2alphame@yahoo.com>
2021-01-28 02:08:00 +0100

    feat(rates): Complete the /api/rates endpoint

--- routes/rates.js
+++ routes/rates.js
@@ -1,8 +1,26 @@
 var express = require('express');
+const https = require('https');
+
 var router = express.Router();
 
-router.get('/rates', function(req, res, next) {
-  res.send('respond with a resource');
+var url = 'https://api.exchangeratesapi.io/latest'
+
+router.get('/', function(req, res, next) {
+
+	url += '?base=' + req.query.base + '&symbols=' + req.query.currency
+	https.get(url, (resp) => {
+		let data = ''
+  		resp.on('data', (chunk) => {
+			data += chunk
+		});
+		resp.on('end', () => {
+			res.json({ results: JSON.parse(data)})
+		})  
+
+	}).on('error', (e) => {
+  		res.status(500).json({error: 'The request could not be completed'})
+	});
+	
 });
 
 module.exports = router;
